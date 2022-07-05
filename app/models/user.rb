61361ed5e5f586d2e616e8821cc727f16dcb2d5c
CARLOSFC <carlos.fuentealba.cruz@gmail.com>
2022-07-04 21:11:29 -0400

    app auth commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ApplicationRecord
+
+include Bcrypt
+validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+#se puede utilizar "has_secure_password, el cual realiza desde la linea 7 hasta la 27"
+#para ejecutar, se reemplaza la linea numero 3
+validate :password_present?
+
+    def password=(new_password)
+        @password = Password.create(new_password)
+        self.password_digest = @password
+    end
+
+    def password
+        return nil unless password_digest.present?
+
+        @password ||= Password.new(password_digest)
+    end
+
+    def authenticate(unencrypte_password)
+        self.password.is_password?(unencrypte_password) && self
+    end
+
+    def password_present?
+        errors.add(:password, :blank) unless password_present?
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+ 
   get 'cookies/set'
   get 'cookies/show'
   get 'cookies/delete'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'home/index'
+
+  resources :sessions, only: %i[new create destroy]
+  root 'home#index'
 end
